1b29afd8df458a42ccbde9915a6e60e1fbc89bf9
bimapriaaditya <bpabima@gmail.com>
2023-10-18 23:04:30 +0700

    add navbar in each file

--- src/routes/app.tsx
+++ src/routes/app.tsx
@@ -1,9 +1,11 @@
 import { A } from "solid-start";
 import Counter from "~/components/Counter";
+import MainNav from "~/components/Nav";
 
 export default function Home() {
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
+      <MainNav />
       <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
         Hello world! 123
       </h1>
